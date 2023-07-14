5c4e281d0046d8e4617e3aad6631ad365e12ae6e
2021vkavuri <60520973+2021vkavuri@users.noreply.github.com>
2023-07-14 07:28:42 -0400

    improved back button and added route label

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import { useState } from 'react';
 import { PrefRoutes, Route, NameToCode } from './models';
 var routes;
 var prefroutes;
+var origin;
+var destination;
 
 function App() {
   const [showFormA, setShowFormA] = useState(true);
   const [showPrefRoutesCard, setShowPrefRoutesCard] = useState(false);
   const [showRouteCardCollection, setShowRouteCardCollection] = useState(false);
   const [showInvalidInput, setShowInvalidInput] = useState(false);
+  const [showBackButton, setShowBackButton] = useState(false)
   
   
 
@@ -33,11 +36,13 @@ function App() {
     try {
       var ori = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+ fields.Field0.replace(" ", "+") +"&key=AIzaSyA8jXrbPQUV4atOUd_50C6aHxKVts1ssUA");
       ori = await ori.json();
+      origin = ori.results[0].address_components[0].long_name;
       ori = await DataStore.query(NameToCode, (c) => c.placeId.eq(ori.results[0].place_id));
       ori = ori[0].code;
     
       var dest = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+ fields.Field1.replace(" ", "+") +"&key=AIzaSyA8jXrbPQUV4atOUd_50C6aHxKVts1ssUA");
       dest = await dest.json();
+      destination = dest.results[0].address_components[0].long_name;
       dest = await DataStore.query(NameToCode, (c) => c.placeId.eq(dest.results[0].place_id));
       dest = dest[0].code;
     } catch (error) {
@@ -48,6 +53,7 @@ function App() {
     setShowFormA(false);
     setShowPrefRoutesCard(true);
     setShowInvalidInput(false);
+    setShowBackButton(true);
   }
 
   function goBack() {
@@ -55,11 +61,14 @@ function App() {
     setShowFormA(true);
     setShowRouteCardCollection(false);
     setShowInvalidInput(false);
+    setShowBackButton(false);
   }
 
   return (
     <>
-      <BackButton paddingTop={ 50 } paddingLeft={ 50 } onClick={() => goBack() } />
+      <div style={{ display : showBackButton === false && 'none' }}>
+        <BackButton paddingTop={ 50 } paddingLeft={ 50 } onClick={() => goBack() } />
+      </div>
       <div className="App" style={{ display : showInvalidInput === false && 'none' }}>
         Please enter a valid input
       </div>
@@ -75,6 +84,7 @@ function App() {
           }})} />
       </div>
       <div className="App" style={{ display : showRouteCardCollection === false && 'none' }}>
+        Route from {origin} to {destination}
         <RouteCardCollection items={routes}/>
       </div>
     </>
